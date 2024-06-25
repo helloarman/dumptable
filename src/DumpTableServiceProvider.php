4bd328c2dddc2dda5c8d098b4dc69df3644185fe
Arman Rahman <armanrahman0@gmail.com>
2024-06-26 02:16:16 +0600

    init: initializing file

---
+++ src/DumpTableServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Helloarman\Dumptable;
+
+use Illuminate\Support\ServiceProvider;
+
+class DumpTableServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
